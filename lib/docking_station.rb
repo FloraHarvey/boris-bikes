7240791fd2ebee1b84ed965e702f3cf5139bb872
Flora Harvey <fharvey555@gmail.com>
2017-02-17 21:13:36 +0000

    Refactored, added test to check capacity can be changed on instantiation

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,36 +2,30 @@ class DockingStation
   DEFAULT_CAPACITY = 20
 
   attr_reader :bikes
-  attr_accessor :capacity
+  attr_reader :capacity
 
   def initialize(capacity = DEFAULT_CAPACITY)
     @bikes = []
     @capacity = capacity
   end
 
-
-  # def capacity(capacity = 20)
-  #     @capacity = capacity
-  # end
-
   def release_bike
-    raise "No bikes available" if empty? 
+    raise "No bikes available" if empty?
     @bikes.each do |bike|
       if bike.working == true
         @bikes.delete(bike)
         return bike
       end
     end
-    raise "No bikes available"
+    raise "No working bikes available"
   end
 
-  def dock_bike(bike, status=true)
-    if status == false
+  def dock_bike(bike, working=true)
+    if !working
       bike.working = false
     end
     raise "Docking station is full" if full?
-  
-    @bikes.push(bike)
+    @bikes << bike
   end
 
 private
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,84 +12,81 @@ describe DockingStation do
   #   end
   # end
 
+  describe 'initialization' do
+    it "instantiates new docking stations with a default capacity of 20" do
+      expect(subject.capacity).to eq 20
+    end
 
-  it "instantiates new docking stations with a default capacity of 20" do
-    expect(subject.capacity).to eq 20
-  end
-
-  it "allows the user to set capacity when instantiating a new docking station" do
-    subject.capacity = 25
-    expect(subject.capacity).to eq 25
+    it "allows the user to set capacity when instantiating a new docking station" do
+      docking_station = DockingStation.new(25)
+      expect(docking_station.capacity).to eq 25
+    end
   end
 
   describe '#release_bike' do
-      it "tests that the release_bike method returns a bike" do
-          bike = Bike.new
-          subject.dock_bike(bike)
-          expect(subject.release_bike).to be_an_instance_of Bike
-      end
-        # uses rspec 'predicate' syntax
-      it "tests that the released bike is working" do
-        bike = Bike.new
-        subject.dock_bike(bike)
-          expect(subject.release_bike.working).to eq true
-      end
-
-      it "tests that docking stations won't release broken bikes" do 
-        bike = Bike.new
-        subject.dock_bike(bike, false)
-        expect {subject.release_bike}.to raise_error("No bikes available")
-      end
-
-      it "tests that docking station won't release broken bikes but will release working bikes" do
-        working_bike = Bike.new
-        broken_bike = Bike.new(false)
-        subject.dock_bike(broken_bike)
-        subject.dock_bike(working_bike)
-        expect(subject.release_bike).to eq working_bike
-
-      end
-
-
-
-      it "raises an error if docking station is empty" do
-        expect {subject.release_bike}.to raise_error("No bikes available")
-      end
-
+    it "tests that the release_bike method returns a bike" do
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect(subject.release_bike).to be_an_instance_of Bike
     end
+    # uses rspec 'predicate' syntax
+    it "tests that the released bike is working" do
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect(subject.release_bike.working).to eq true
+    end
+
+    it "tests that docking stations won't release broken bikes" do
+      bike = Bike.new
+      subject.dock_bike(bike, false)
+      expect {subject.release_bike}.to raise_error("No working bikes available")
+    end
+
+    it "tests that docking station won't release broken bikes but will release working bikes" do
+      working_bike = Bike.new
+      broken_bike = Bike.new(false)
+      subject.dock_bike(broken_bike)
+      subject.dock_bike(working_bike)
+      expect(subject.release_bike).to eq working_bike
+    end
+
+    it "raises an error if docking station is empty" do
+      expect {subject.release_bike}.to raise_error("No bikes available")
+    end
+
+  end
 
   describe '#dock_bike' do
     it "raises an error if docking station is full" do
       (subject.capacity).times do
         subject.dock_bike(Bike.new)
       end
-      expect {subject.dock_bike(Bike.new)}.to raise_error("Docking station is full")
-    end
-    
-    it "update status if in broken condition" do 
-     bike = Bike.new
-     subject.dock_bike(bike,false)
-     expect(bike.working).to eq false
+    expect {subject.dock_bike(Bike.new)}.to raise_error("Docking station is full")
     end
 
+    it "updates working status if broken" do
+      bike = Bike.new
+      subject.dock_bike(bike,false)
+      expect(bike.working).to eq false
+    end
 
-            describe '#dock_bike returns bikes' do            
-              before :each do
-                @bike = Bike.new
-                subject.dock_bike(@bike)
-              end
-              it "shows a bike that has been docked" do
-                expect(subject.bikes).to eq([@bike])
-              end
+    describe '#dock_bike returns bikes' do
+      before :each do
+        @bike = Bike.new
+        subject.dock_bike(@bike)
+      end
+      it "shows a bike that has been docked" do
+        expect(subject.bikes).to eq([@bike])
+      end
 
-              it "returns an array" do
-                expect(subject.bikes).to be_kind_of(Array)
-              end
+      it "returns an array" do
+        expect(subject.bikes).to be_kind_of(Array)
+      end
 
-              it "adds a new bike to the end of the array" do
-                expect(subject.bikes[-1]).to eq(@bike)
-              end
-            end
+      it "adds a new bike to the end of the array" do
+        expect(subject.bikes[-1]).to eq(@bike)
+      end
+    end
 
   end
 end
